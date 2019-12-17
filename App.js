e3fa46dfa8267970a2d3de7896e266a76485706f
lester.dominguez@gmail.com <Lester.dominguez@gmail.com>
2019-12-17 13:08:26 -0500

    Moved camera permissions to App.js file

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
 import { AppLoading } from 'expo';
 import { Asset } from 'expo-asset';
 import * as Font from 'expo-font';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import { Camera } from 'expo-camera';
 
 import AppNavigator from './navigation/AppNavigator';
 
 export default function App(props) {
   const [isLoadingComplete, setLoadingComplete] = useState(false);
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type] = useState(Camera.Constants.Type.back);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
 
   if (!isLoadingComplete && !props.skipLoadingScreen) {
     return (
